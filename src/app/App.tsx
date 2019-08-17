621bf1a4f124d0b23f0dc89af1b0b349a8b1f38b
Rugal Bernstein <this@rug.al>
2019-08-17 01:17:39 -0400

    Add styled component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-import "./App.css";
+
 import logo from "./logo.svg";
+import { Header, Link, Logo, Main } from "./style";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Main>
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <Link
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </Header>
+    </Main>
   );
 };
 
