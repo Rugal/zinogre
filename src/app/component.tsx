ae10126f23b7ef1e6b28e709585b74451ea04c6a
Rugal Bernstein <this@rug.al>
2019-08-19 21:24:10 -0400

    Formulate react-archer

--- src/app/component.tsx
+++ src/app/component.tsx
@@ -1,28 +1,67 @@
 import React from "react";
 
-import logo from "./logo.svg";
-import { GlobalStyle, Header, Link, Logo, Main } from "./style";
+import { ArcherContainer, ArcherElement, Relation } from "react-archer";
 
-const App: React.FC = () => {
+const rootStyle = { display: "flex", justifyContent: "center" };
+const rowStyle = { margin: "200px 0", display: "flex", justifyContent: "space-between" };
+const boxStyle = { padding: "10px", border: "1px solid black" };
+const relationships: Relation[] = [{
+  sourceAnchor: "bottom",
+  targetAnchor: "top",
+  targetId: "element2",
+}, {
+  sourceAnchor: "bottom",
+  targetAnchor: "top",
+  targetId: "element3",
+}, {
+  sourceAnchor: "bottom",
+  targetAnchor: "top",
+  targetId: "element4",
+}, {
+  sourceAnchor: "bottom",
+  targetAnchor: "top",
+  targetId: "element5",
+}, {
+  sourceAnchor: "bottom",
+  targetAnchor: "top",
+  targetId: "element6",
+}];
+
+const App = () => {
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <Main>
-        <Header>
-          <Logo src={logo} alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <Link
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Link>
-        </Header>
-      </Main>
-    </React.Fragment>
+    <div>
+
+      <ArcherContainer strokeColor="red" >
+        <div style={rootStyle}>
+          <ArcherElement id="root" relations={relationships}>
+            <div style={boxStyle}>Root</div>
+          </ArcherElement>
+        </div>
+
+        <div style={rowStyle}>
+          <ArcherElement id="element2">
+            <div style={boxStyle}>Element 2</div>
+          </ArcherElement>
+
+          <ArcherElement id="element3">
+            <div style={boxStyle}>Element 3</div>
+          </ArcherElement>
+
+          <ArcherElement id="element4">
+            <div style={boxStyle}>Element 4</div>
+          </ArcherElement>
+
+          <ArcherElement id="element5">
+            <div style={boxStyle}>Element 5</div>
+          </ArcherElement>
+
+          <ArcherElement id="element6">
+            <div style={boxStyle}>Element 6</div>
+          </ArcherElement>
+        </div>
+      </ArcherContainer>
+
+    </div>
   );
 };
 
