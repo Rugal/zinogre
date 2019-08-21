bc5357b9980f58dab585c9e04e5ff9c6227a6765
Rugal Bernstein <this@rug.al>
2019-08-21 00:43:47 -0400

    Use material-ui

--- src/app/component.tsx
+++ src/app/component.tsx
@@ -1,111 +1,45 @@
+import {
+  AppBar,
+  Button,
+  createMuiTheme,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { blue, pink } from "@material-ui/core/colors";
+import CategoryIcon from "@material-ui/icons/Category";
+import ContactsIcon from "@material-ui/icons/Contacts";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+import LabelIcon from "@material-ui/icons/Label";
+import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 
-import { ArcherContainer, ArcherElement, Relation } from "react-archer";
+import { GlobalStyle, style } from "./style";
 
-const rootStyle = { display: "flex", justifyContent: "center", backgroundColor: "blue" };
-const rowStyle = { margin: "200px 0", display: "flex", justifyContent: "space-between", backgroundColor: "red" };
-const rowStyle1 = { margin: "200px 0", display: "flex", justifyContent: "space-between", backgroundColor: "yellow" };
-const boxStyle = { padding: "10px", border: "1px solid black" };
-const relationships: Relation[] = [{
-  sourceAnchor: "bottom",
-  targetAnchor: "top",
-  targetId: "element2",
-}, {
-  sourceAnchor: "bottom",
-  targetAnchor: "top",
-  targetId: "element3",
-}, {
-  sourceAnchor: "bottom",
-  targetAnchor: "top",
-  targetId: "element4",
-}];
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+  },
+});
 
-const relationships1: Relation[] = [{
-  sourceAnchor: "bottom",
-  targetAnchor: "top",
-  targetId: "element5",
-}, {
-  sourceAnchor: "bottom",
-  targetAnchor: "top",
-  targetId: "element6",
-}];
+export default function App() {
 
-const relationships2: Relation[] = [{
-  sourceAnchor: "bottom",
-  targetAnchor: "top",
-  targetId: "element7",
-}, {
-  sourceAnchor: "bottom",
-  targetAnchor: "top",
-  targetId: "element8",
-}];
+  const classes = style();
 
-const App = () => {
   return (
-    <div>
-
-      <ArcherContainer strokeColor="red" >
-        <div style={rootStyle}>
-          <ArcherElement id="root" relations={relationships}>
-            <div style={boxStyle}>Root</div>
-          </ArcherElement>
-        </div>
-
-        <div style={rowStyle}>
-          <ArcherElement id="element2">
-            <div style={boxStyle}>Element 2</div>
-          </ArcherElement>
-
-          <ArcherElement id="element3" relations={relationships1}>
-            <div style={boxStyle}>Element 3</div>
-          </ArcherElement>
-
-          <ArcherElement id="element4" relations={relationships2}>
-            <div style={boxStyle}>Element 4</div>
-          </ArcherElement>
-        </div>
-
-        <div style={rowStyle1}>
-          <ArcherElement id="element5">
-            <div style={boxStyle}>Element 5</div>
-          </ArcherElement>
-
-          <ArcherElement id="element6">
-            <div style={boxStyle}>Element 6</div>
-          </ArcherElement>
-
-          <ArcherElement id="element7">
-            <div style={boxStyle}>Element 7</div>
-          </ArcherElement>
-
-          <ArcherElement id="element8">
-            <div style={boxStyle}>Element 8</div>
-          </ArcherElement>
-
-          <ArcherElement id="element8">
-            <div style={boxStyle}>Element 8</div>
-          </ArcherElement>
-
-          <ArcherElement id="element9">
-            <div style={boxStyle}>Element 8</div>
-          </ArcherElement>
-
-          <ArcherElement id="element10">
-            <div style={boxStyle}>Element 8</div>
-          </ArcherElement>
-
-          <ArcherElement id="element11">
-            <div style={boxStyle}>Element 8</div>
-          </ArcherElement>
-
-          <ArcherElement id="element12">
-            <div style={boxStyle}>Element 8</div>
-          </ArcherElement>
-        </div>
-      </ArcherContainer>
-
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <GlobalStyle />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>Rugal</Typography>
+            <Button color="inherit"><ContactsIcon className={classes.icon} /></Button>
+            <Button color="inherit"><CategoryIcon className={classes.icon} /></Button>
+            <Button color="inherit"><LabelIcon className={classes.icon} /></Button>
+            <Button color="inherit"><FormatListBulletedIcon className={classes.icon} /></Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
-};
-
-export default App;
+}
--- src/app/style.tsx
+++ src/app/style.tsx
@@ -1,41 +1,11 @@
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
 // eslint-disable-next-line
 import React from "react";
-import styled, { createGlobalStyle, keyframes } from "styled-components";
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-export const Main = styled.div`
-  text-align: center;
-`;
-
-export const Logo = styled.img`
-  animation: ${rotate} infinite 20s linear;
-  height: 40vmin;
-  pointer-events: none;
-`;
-
-export const Header = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-export const Link = styled.a`
-  color: #61dafb;
-`;
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
@@ -48,7 +18,24 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
   }
 `;
+
+export const style = makeStyles((theme: Theme) =>
+  createStyles({
+    icon: {
+      fontSize: 32,
+      margin: theme.spacing(1),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
