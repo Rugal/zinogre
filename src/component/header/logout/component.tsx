348568b988f7069fa34d7c5a00b7afd49256e275
Rugal Bernstein <rugal.bernstein@thomsonreuters.com>
2020-01-13 13:01:25 +0800

    Go back to index after logout

--- src/component/header/logout/component.tsx
+++ src/component/header/logout/component.tsx
@@ -6,6 +6,7 @@ import {
   DialogTitle,
 } from "@material-ui/core";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import { transparency } from "../common";
 
@@ -20,13 +21,18 @@ interface IProps {
 
 const Logout: React.FC<IProps> = (p) => {
 
+  const history = useHistory();
+  const backToIndex = () => history.push("/");
+
   const logout = () => {
     // 1. show progress bar
     p.show();
     setTimeout(() => {
-      // 2. logout & clean
+      // 2. go back to history
+      backToIndex();
+      // 3. logout & clean
       p.logout();
-      // 3. clean & close
+      // 4. clean & close
       p.handleClose();
       setTimeout(() => {
         p.hide();
@@ -39,12 +45,8 @@ const Logout: React.FC<IProps> = (p) => {
     : <div>
       <DialogTitle id="form-dialog-title">Confirmation</DialogTitle>
       <DialogActions>
-        <Button color="primary" onClick={p.handleClose}>
-          Cancel
-          </Button>
-        <Button color="secondary" onClick={logout}>
-          Confirm
-          </Button>
+        <Button color="primary" onClick={p.handleClose}>Cancel</Button>
+        <Button color="secondary" onClick={logout}>Confirm</Button>
       </DialogActions>
     </div>
     ;
